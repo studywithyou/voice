94a6594dbbf434589b031f5ea8eef8be913e5ab2
Peter Twickler <peter.twickler@gmail.com>
2014-03-30 00:41:31 -0400

    just some setup files: sample jpgs and an empty box html with a css sheet linked in only one style just to verify the link

---
+++ object.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!doctype html>
+  <html lang="en">
+    <head>
+      <title>
+        Object 0ne
+      </title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="./object.css">
+
+    </head>
+    <body>
+      <div>
+        Goo
+      </div>
+    </body>
+  </html>
